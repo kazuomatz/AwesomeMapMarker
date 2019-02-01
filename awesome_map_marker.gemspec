9c6f6e82197ef3374892ffc4150058fb01d925fa
松永和男 <matsunaga@iarchitect.co.jp>
2019-02-01 19:59:15 +0900

    first commit

--- awesome_map_marker.gemspec
+++ awesome_map_marker.gemspec
@@ -6,22 +6,22 @@ require "awesome_map_marker/version"
 Gem::Specification.new do |spec|
   spec.name          = "awesome_map_marker"
   spec.version       = AwesomeMapMarker::VERSION
-  spec.authors       = ["松永和男"]
-  spec.email         = ["matsunaga@iarchitect.co.jp"]
+  spec.authors       = ["Kazuo Matsunaga"]
+  spec.email         = ["getlasterror@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Map Marker generator with Fontawesome}
+  spec.description   = %q{Simple Map Marker generator with Fontawesome}
+  spec.homepage      = "https://github.com/kazuomatz"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata["allowed_push_host"] = "https://github.com/kazuomatz"
 
     spec.metadata["homepage_uri"] = spec.homepage
-    spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-    spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+    spec.metadata["source_code_uri"] = "https://github.com/kazuomatz/AwesomeMapMarker"
+    spec.metadata["changelog_uri"] = "https://github.com/kazuomatz/AwesomeMapMarker/CHANGELOG.md"
   else
     raise "RubyGems 2.0 or newer is required to protect against " \
       "public gem pushes."
@@ -39,4 +39,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  spec.add_runtime_dependency 'mini_magick', '~> 4.8'
+  spec.add_runtime_dependency 'font_awesome5_rails'
 end
--- lib/awesome_map_marker.rb
+++ lib/awesome_map_marker.rb
@@ -1,6 +1,103 @@
-require "awesome_map_marker/version"
+require 'awesome_map_marker/version'
+require 'mini_magick'
+require 'yaml'
 
 module AwesomeMapMarker
   class Error < StandardError; end
   # Your code goes here...
+  #
+  def self.greet
+    p 'Hello'
+  end
+
+  class PingConverter
+
+    def initialize(size: 128, type: "far", fill_color:'#000000',threshold: 0.01)
+      @width = size
+      @height = size
+      @font_size = (size * 0.76).ceil
+      @font_path = self.font_path()
+      @char_position = { x: @width * 0.53, y: (@font_size + @height) * 0.45 }
+      @threshold = threshold
+      @fill_color = fill_color
+    end
+
+
+    def draw_char(draw, image, char, char_position = @char_position)
+      draw.font = @font_path
+      draw.fill = '#ffffff'
+      draw.gravity = Magick::CenterGravity
+      draw.stroke = 'transparent'
+      draw.pointsize = @font_size
+      draw.text_antialias = true
+      draw.kerning = 0
+      draw.text_align(Magick::CenterAlign)
+      draw.text(char_position[:x], char_position[:y], char)
+      draw.draw(image)
+    end
+
+
+    def generate(name)
+      icon_data = icon_data(name)
+      if icon_data
+        unicode = icon_data['unicode']
+        char = [Integer("0x#{unicode}")].pack('U*')
+        image = Magick::Image.new(@width, @height)
+        image = image.matte_replace(0, 0)
+        draw_char(draw, image, char)
+        return image
+      else
+        return nil
+      end
+
+    end
+
+    def icon_data(name)
+      icons_data_yml.map { |icon| icon[:id] === name ?  icon : nil }.compact.first
+    end
+
+    def icons_data_yml(type)
+      yml_path = File.expand_path("../config/#{type}.yml",__FILE__)
+      YAML.load_file(yml_path)
+    end
+
+    def self.icon_type(type)
+      return "fas" if type.nil?
+      case type.to_s
+      when "far", "regular"
+        "far"
+      when "fab", "brand"
+        "fab"
+      else
+        "fas"
+      end
+    end
+
+    def self.font_path(type)
+      path = File.expand_path("../app/assets/font",__FILE__)
+      case icon_type(type)
+      when "far"
+        file.join(path,"fa-regular-400.ttf")
+      when "fab"
+        file.join(path,"fa-brands-400.ttf")
+      else  #fas
+        file.join(path,"fa-solid-900.ttf")
+      end
+    end
+
+
+
+
+    def self.blob(type)
+
+
+
+
+
+    end
+
+
+
+  end
+
 end
